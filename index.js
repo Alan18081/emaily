86c8d5b2a9c536a3718faf0d1aefd61f68067e9a
Alex Ostapyuk <gogunov00@gmail.com>
2018-03-07 21:47:53 +0200

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send({name: 'Alan'});
+  res.send({name: 'New name'});
 });
 
 const PORT = process.env.PORT || 5000;
