92b6239359e54e40fd2233df2c2c40eca247c8da
Alex Ostapyuk <gogunov00@gmail.com>
2018-03-08 19:20:28 +0200

    comm

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cookiesSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 
-app.get('/', (req,res) => {
-  res.send({name: 'New name'});
-});
+mongoose.connect(keys.mongoURI);
+require('./models/User');
+
+const app = express();
+app.use(
+  cookiesSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./services/passport');
+require('./routes/auth')(app);
+
 
 const PORT = process.env.PORT || 5000;
 
