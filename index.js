866f367a1ddc0229e53b52af7ea7bba547ec7795
Alex Ostapyuk <gogunov00@gmail.com>
2018-03-11 00:50:17 +0200

    h

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ require('./routes/auth')(app);
 require('./routes/billing')(app);
 
 if(process.env.NODE_ENV === 'production') {
-  // app.use(express.static('client/build'));
+  app.use(express.static('client/build'));
   const path = require('path');
   app.get('*', (req,res) => {
-    res.sendFile('Hewllo');
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
   });
 }
 
