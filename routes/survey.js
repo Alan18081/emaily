797c416b46fc7ffad8eac30ce3c47268127906bf
Alex Ostapyuk <gogunov00@gmail.com>
2018-03-13 23:04:33 +0200

    heqey

--- routes/survey.js
+++ routes/survey.js
@@ -28,7 +28,8 @@ module.exports = app => {
         if(match) {
           return {
             email,
-            ...match
+            surveyId: match.surveyId,
+            choice: match.choice
           };
         }
       })
