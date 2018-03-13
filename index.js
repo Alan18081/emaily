b2bdf057fe02061df9de8d06a0065442102ebd0a
Alex Ostapyuk <gogunov00@gmail.com>
2018-03-13 22:58:22 +0200

    new

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ require('./routes/survey')(app);
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname,'client','build')));
-  app.get('/*', (req,res) => {
+  app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname,'client','build','main.html'));
   });
 }
